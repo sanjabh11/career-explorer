11957cbac827c8f7480d4184b149bf8a35df423d
sanjabh11 <33122397+sanjabh11@users.noreply.github.com>
2024-08-18 19:26:18 +0530

    Update JobTaxonomySelector.js
    Signed-off-by: sanjabh11 <33122397+sanjabh11@users.noreply.github.com>

--- src/components/JobTaxonomySelector.js
+++ src/components/JobTaxonomySelector.js
@@ -124,19 +124,7 @@ const JobTaxonomySelector = () => {
     }
   };
 
-  const handleFileUpload = (event) => {
-    const file = event.target.files[0];
-    const reader = new FileReader();
-    reader.onload = (e) => {
-      const data = new Uint8Array(e.target.result);
-      const workbook = XLSX.read(data, { type: 'array' });
-      const sheetName = workbook.SheetNames[0];
-      const worksheet = workbook.Sheets[sheetName];
-      //const jsonData = XLSX.utils.sheet_to_json(worksheet);
-      // You can use jsonData here if needed
-    };
-    reader.readAsArrayBuffer(file);
-  };
+  
 
   const renderList = (title, items, category) => {
     if (!items || items.length === 0) {
