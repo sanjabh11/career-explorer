e30d941082b555c0adf5f35852fb6cdff895d8a4
sanjabh11 <33122397+sanjabh11@users.noreply.github.com>
2024-08-18 15:04:31 +0530

    Update OnetService.js
    Signed-off-by: sanjabh11 <33122397+sanjabh11@users.noreply.github.com>

--- src/services/OnetService.js
+++ src/services/OnetService.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: process.env.REACT_APP_API_BASE_URL || '',
+  baseURL: process.env.REACT_APP_API_BASE_URL || '/api',
+  axios.get(`${apiBaseUrl}/onet-search`)
 });
 
 export const searchOccupations = async (keyword) => {
